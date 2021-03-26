a0f3c438cd391260581edda0223930c719628d91
Saulo <sauloaguiar89@gmail.com>
2021-03-26 18:06:36 -0300

    Creates route table and internet gateway for vpc

--- main.tf
+++ main.tf
@@ -1,69 +1,58 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-1"
   # never put sensitive data in the configuration file
   # refer to the readme file on how to use it
-  # access_key = "value"
-  # secret_key = "value"
+  #access_key = "AKIAQKXWVPUTOANFBR6S"
+  #secret_key = "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"
 }
+variable "vpc_cidr_blocks" {}
+variable "subnet_cidr_blocks" {}
 
-variable "subnet_cidr_block" {
-  description = "subnet cidr block"
-  // default value will be use if none present on file
-  // it will be overwriten if specified by the file
-  default = "10.0.50.0/24" 
+variable "avail_zone" {}
 
-  // types are usually not required
-  // unless we want to enforce the type in the documentation for use
-  type = string
-}
+variable "env_prefix" {}
 
-variable "cidr_blocks" {
-  description = "cidr blocks for vpcs and subnets"
-  # type = list(string)
-  // use when other team members re use configuration files
-  type = list(object({cidr_block = string, name = string}))
-}
-
-
-resource "aws_vpc" "development-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_blocks
   
   // adding or removing attributes will add/remove them in the aws resource automatically
   tags = {
-    Name = var.cidr_blocks[0].name,
-    vpc_env= "dev"
+    Name = "${var.env_prefix}-vpc",
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
+resource "aws_subnet" "myapp-subnet-1" {
   // define to which vpc this subnet should belong
   // each resource has its own object equivalent
-  vpc_id = aws_vpc.development-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block // index is 0 based
-  availability_zone = "us-east-1a"
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_blocks
+  availability_zone = var.avail_zone
   tags = {
-    Name = var.cidr_blocks[0].name
+    Name = "${var.env_prefix}-subnet-1",
   }
 }
 
-// adding new resources to a pre existing resource
-data  "aws_vpc" "existing_vpc" {
-  default = true
-}
-
-resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing_vpc.id
-  cidr_block = "10.0.10.0/24" // use cidr according to the cidr block from the vpc being used
-  availability_zone = "us-east-1a"
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
   tags = {
-    Name = "subnet-2-default"
+    Name = "${var.env_prefix} internet gateway"
   }
 }
 
-// these will be printed after the terraform apply execution ends
-output "dev-vpc-id" {
-  value = aws_vpc.development-vpc.id
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix} route table"
+  }
 }
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+
+resource "aws_route_table_association" "route-table-association-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
